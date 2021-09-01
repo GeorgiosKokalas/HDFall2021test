b794bf6ab2a83289a00d7b21440ab67b812e29df
GeorgiosKokalas <45497707+GeorgiosKokalas@users.noreply.github.com>
2021-09-01 11:24:07 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,16 +4,16 @@
 echo "C testing"
 echo "---------"
 echo "  -Compiling"
-gcc ctest.c -o ctest 2> err.txt
+gcc ctest.c -o ctest 2> err.txt > /dev/null
 echo "  -Running"
-./ctest 2>> err.txt 1> res.txt
+./ctest 2>> err.txt 1> res.txt > /dev/null
 
 
 echo ""
 echo "Assembly Testing"
 echo "----------------"
 echo "  -Compiling"
-gcc atest.s -o atest 2>> err.txt
+gcc atest.s -o atest 2>> err.txt > /dev/null
 echo "  -Running"
 ./atest 2>> err.txt 1>> res.txt
 
@@ -22,9 +22,9 @@ echo ""
 echo "MPI testing"
 echo "-----------"
 echo "  -Compiling"
-cd MPI && make 2>> ../err.txt
+cd MPI && make 2>> ../err.txt > /dev/null
 echo "  -Running"
-./masterWorker 2>> ../err.txt 1>> ../res.txt
+./masterWorker 2>> ../err.txt 1>> ../res.txt > /dev/null
 cd ..
 
 
@@ -32,9 +32,9 @@ echo ""
 echo "OpenMP testing"
 echo "-----------"
 echo "  -Compiling"
-cd OpenMP && make 2>> ../err.txt
+cd OpenMP && make 2>> ../err.txt > /dev/null
 echo "  -Running"
-./reduction 2>> ../err.txt 1>> ../res.txt
+./reduction 2>> ../err.txt 1>> ../res.txt > /dev/null
 cd ..
 
 
@@ -42,13 +42,46 @@ echo ""
 echo "hybrid testing"
 echo "-----------"
 echo "  -Compiling"
-cd hybrid && make 2>> ../err.txt
+cd hybrid && make 2>> ../err.txt > /dev/null
 echo "  -Running"
-./spmd 2>> ../err.txt 1>> ../res.txt
+./spmd 2>> ../err.txt 1>> ../res.txt > /dev/null
 cd ..
 
 
+everythingWorks=true
 
+# Search for lines in ~/outtest.txt
+while IFS= read -r line
+do
+  if ! grep "$line" res.txt > /dev/null
+  then
+    echo -e "\e[1;31mFOLLOWING LINE NOT DETECTED: $line \e[0m"
+    everythingWorks=false
+  fi
+done < rescheck.txt
+
+#Output if all of the Patternlets worked
+if $everythingWorks 
+then
+  echo -e "\e[1;32mTesting showed no errors \e[0m"
+else
+  echo -e "\e[1;33mTESTING RESULTED IN ERROR\e[0m"
+fi
+
+# In case any of the Patternlets didn't work
+if ! $everythingWorks
+then
+  if [ ! -s err.txt ]
+  then
+    echo -e "\e[1;33mNO ERROR MESSAGES\e[0m"
+  else
+    echo -e "\e[1;31mERROR MESSAGES PRESENT \e[0m"
+    echo "Showing error output"
+    echo "######################"
+    cat err.txt
+
+  fi
+fi
 
 
 
