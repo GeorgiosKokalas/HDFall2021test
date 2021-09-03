133d72b9a3020cbd0dd9cb7922fae8876a1bd3b3
GeorgiosKokalas <45497707+GeorgiosKokalas@users.noreply.github.com>
2021-09-03 14:06:52 -0500

    Update ctest.c

--- ctest.c
+++ ctest.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main() {
-  printf("Hello world!\n");
-  return 0;
+int getbit(int val, int bitno){
+        return ((val >> bitno) & 1);
 }
+
+
+int main (){
+        printf("Hello, World!\n");
+        int input=0xa;
+        printf("%x = %d\n", input, input);
+        for (int i=7; i>=0; i--){
+                printf("%d ", getbit(input, i));
+        }
+        printf("\n");
+        return 0;
+}
+
