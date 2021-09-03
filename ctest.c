0ed5832d8b3121d7eb78df68a9902369832ae3a3
GeorgiosKokalas <45497707+GeorgiosKokalas@users.noreply.github.com>
2021-09-03 14:08:26 -0500

    Update ctest.c

--- ctest.c
+++ ctest.c
@@ -8,7 +8,7 @@ int getbit(int val, int bitno){
 int main (){
         printf("Hello, World!\n");
         int input=0xa;
-        printf("%x = %d\n", input, input);
+        if(input == 10){ printf("%x = %d\n", input, input);}
         for (int i=7; i>=0; i--){
                 printf("%d ", getbit(input, i));
         }
